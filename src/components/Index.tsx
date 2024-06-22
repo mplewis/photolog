09b581c90d43f21633fc16133c61c0b0ebb2fe6c
Matt Lewis <matt@mplewis.com>
2024-06-22 16:14:47 -0600

    feat: parse and display metadata

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,9 +1,13 @@
 import YARL, { type SlideImage } from "yet-another-react-lightbox";
+import Captions from "yet-another-react-lightbox/plugins/captions";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import { useState } from "react";
 import classNames from "classnames";
+import { stripIndents } from "common-tags";
+import dayjs from "dayjs";
 
 import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import type { OriginalMetadata } from "../types";
 
@@ -60,14 +64,36 @@ const gridSourceSizes = screenSizes
   )
   .join(",\n");
 
+function trimSp(s: string): string {
+  return s.replace(/\s+/g, " ").trim();
+}
+
+function trimCommonPrefixWords(base: string, toTrim: string): string {
+  const baseWords = base.split(" ");
+  const toTrimWords = toTrim.split(" ");
+  for (let i = 0; i < baseWords.length && i < toTrimWords.length; i++) {
+    if (baseWords[i] !== toTrimWords[i]) return toTrimWords.slice(i).join(" ");
+  }
+  return toTrimWords.slice(baseWords.length).join(" ");
+}
+
 const Index = ({ albums }: { albums: Record<string, Album> }) => {
   const albumToPhotoset: Record<string, Photoset[]> = { _all: [] };
-  for (const { name, photosets: photos } of Object.values(albums)) {
+  for (const { name, photosets } of Object.values(albums)) {
     albumToPhotoset[name] = [];
-    for (const [, photoset] of Object.entries(photos)) {
+    for (const [, photoset] of Object.entries(photosets)) {
       albumToPhotoset[name]!.push(photoset);
       albumToPhotoset._all!.push(photoset);
     }
+  }
+
+  // Sort all albums by date descending
+  for (const album of Object.values(albumToPhotoset)) {
+    album.sort((a, b) => {
+      if (!a.metadata.date) return -1;
+      if (!b.metadata.date) return 1;
+      return b.metadata.date.getTime() - a.metadata.date.getTime();
+    });
   }
 
   const [current, setCurrent] = useState<{ album: string; photos: Photoset[] }>(
@@ -157,33 +183,77 @@ const Index = ({ albums }: { albums: Record<string, Album> }) => {
 
   const Lightbox = () => {
     if (!current) return;
+
     const slides: SlideImage[] = current.photos.map((photoset) => {
       if (!photoset.sizes[YARL_THUMBNAIL_SIZE])
         throw new Error(
           `Missing ${YARL_THUMBNAIL_SIZE} size for ${JSON.stringify(photoset)}`
         );
+
+      const m = photoset.metadata;
+
+      const t: string[] = [];
+      if (m.title) t.push(m.title);
+      if (m.location) t.push(m.location);
+      const title = t.join(", ");
+
+      const camera = trimSp(`${m.cameraMake || ""} ${m.cameraModel || ""}`);
+      let lens = trimSp(`${m.lensMake || ""} ${m.lensModel || ""}`);
+      lens = trimCommonPrefixWords(camera, lens);
+
+      const s: string[] = [];
+      if (m.exposureTime) s.push(`${m.exposureTime}s`);
+      if (m.fNumber) s.push(`f/${m.fNumber}`);
+      if (m.iso) s.push(`ISO ${m.iso}`);
+      const settings = s.join(", ");
+
+      const d: string[] = [];
+      if (camera) d.push(camera);
+      if (lens) d.push(lens);
+      if (settings) d.push(settings);
+      const details = d.join(", ");
+
+      const description = [
+        // photoset.sizes[YARL_THUMBNAIL_SIZE].url,
+        m.description,
+        dayjs(m.date).format("dddd, MMMM D, YYYY"),
+        details,
+      ]
+        .filter(Boolean)
+        .join("\n")
+        .trim();
+
+      const srcSet = fullSizes.map(({ size }) => {
+        const src = photoset.sizes[size];
+        if (!src)
+          throw new Error(
+            `Missing ${size} size for ${JSON.stringify(photoset)}`
+          );
+        return { src: src.url, width: src.width, height: src.height };
+      });
+
       return {
+        title,
+        description,
+        srcSet,
         src: photoset.sizes[YARL_THUMBNAIL_SIZE].url,
         width: photoset.sizes[YARL_THUMBNAIL_SIZE].width,
         height: photoset.sizes[YARL_THUMBNAIL_SIZE].height,
-        srcSet: fullSizes.map(({ size }) => {
-          const src = photoset.sizes[size];
-          if (!src)
-            throw new Error(
-              `Missing ${size} size for ${JSON.stringify(photoset)}`
-            );
-          return { src: src.url, width: src.width, height: src.height };
-        }),
       };
     });
 
     return (
       <YARL
-        plugins={[Thumbnails]}
+        plugins={[Captions, Thumbnails]}
         open={selected !== null}
         close={() => setSelected(null)}
         index={selected ?? 0}
         slides={slides}
+        captions={{
+          showToggle: true,
+          descriptionMaxLines: 8,
+          descriptionTextAlign: "center",
+        }}
         thumbnails={{
           border: 0,
           showToggle: true,
