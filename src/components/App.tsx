0d6f2fcd4b2b93885f376492caf6c1d9173389db
Matt Lewis <matt@mplewis.com>
2024-06-24 16:00:30 -0600

    refactor: extract Lightbox

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,71 +1,10 @@
-import YARL, { type SlideImage } from "yet-another-react-lightbox";
-import Captions from "yet-another-react-lightbox/plugins/captions";
-import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import { useState } from "react";
-import dayjs from "dayjs";
 
-import type { Album, OriginalMetadata, Photoset } from "../types";
-import { YARL_THUMBNAIL_SIZE, fullSizes } from "../sizes";
+import type { Album, Photoset } from "../types";
 
 import Nav from "./Nav";
 import Gallery from "./Gallery";
-
-import "yet-another-react-lightbox/styles.css";
-import "yet-another-react-lightbox/plugins/captions.css";
-import "yet-another-react-lightbox/plugins/thumbnails.css";
-
-function trimSp(s: string): string {
-  return s.replace(/\s+/g, " ").trim();
-}
-
-function trimCommonPrefixWords(base: string, toTrim: string): string {
-  const baseWords = base.split(" ");
-  const toTrimWords = toTrim.split(" ");
-  for (let i = 0; i < baseWords.length && i < toTrimWords.length; i++) {
-    if (baseWords[i] !== toTrimWords[i]) return toTrimWords.slice(i).join(" ");
-  }
-  return toTrimWords.slice(baseWords.length).join(" ");
-}
-
-function prettyMeta(m: OriginalMetadata): {
-  title: string;
-  description: string;
-} {
-  const t: string[] = [];
-  if (m.title) t.push(m.title);
-  if (m.location) t.push(m.location);
-  const title = t.join(", ");
-
-  const camera = trimSp(`${m.cameraMake || ""} ${m.cameraModel || ""}`);
-  let lens = trimSp(`${m.lensMake || ""} ${m.lensModel || ""}`);
-  lens = trimCommonPrefixWords(camera, lens);
-
-  const s: string[] = [];
-  if (m.exposureTime) s.push(`${m.exposureTime}s`);
-  if (m.fNumber) s.push(`f/${m.fNumber}`);
-  if (m.iso) s.push(`ISO ${m.iso}`);
-  const settings = s.join(", ");
-
-  const d: string[] = [];
-  if (camera) d.push(camera);
-  if (lens) d.push(lens);
-  if (settings) d.push(settings);
-  const details = d.join(", ");
-
-  if (!m.date) throw new Error(`Missing date for ${JSON.stringify(m)}`);
-
-  const description = [
-    // photoset.sizes[YARL_THUMBNAIL_SIZE].url,
-    m.description && m.description + "\n",
-    dayjs(m.date).format("dddd, MMMM D, YYYY"),
-    details,
-  ]
-    .filter(Boolean)
-    .join("\n")
-    .trim();
-
-  return { title, description };
-}
+import Lightbox from "./Lightbox";
 
 const App = ({ albums }: { albums: Record<string, Album> }) => {
   const albumToPhotoset: Record<string, Photoset[]> = { _all: [] };
@@ -97,60 +36,6 @@ const App = ({ albums }: { albums: Record<string, Album> }) => {
     setCurrent({ album: name, photos });
   }
 
-  const Lightbox = () => {
-    if (!current) return;
-
-    const slides: SlideImage[] = current.photos.map((photoset) => {
-      if (!photoset.sizes[YARL_THUMBNAIL_SIZE])
-        throw new Error(
-          `Missing ${YARL_THUMBNAIL_SIZE} size for ${JSON.stringify(photoset)}`
-        );
-
-      const { title, description } = prettyMeta(photoset.metadata);
-
-      const srcSet = fullSizes.map(({ size }) => {
-        const src = photoset.sizes[size];
-        if (!src)
-          throw new Error(
-            `Missing ${size} size for ${JSON.stringify(photoset)}`
-          );
-        return { src: src.url, width: src.width, height: src.height };
-      });
-
-      return {
-        title,
-        description,
-        srcSet,
-        src: photoset.sizes[YARL_THUMBNAIL_SIZE].url,
-        width: photoset.sizes[YARL_THUMBNAIL_SIZE].width,
-        height: photoset.sizes[YARL_THUMBNAIL_SIZE].height,
-      };
-    });
-
-    return (
-      <YARL
-        plugins={[Captions, Thumbnails]}
-        open={selected !== null}
-        close={() => setSelected(null)}
-        index={selected ?? 0}
-        slides={slides}
-        captions={{
-          showToggle: true,
-          descriptionMaxLines: 8,
-          descriptionTextAlign: "center",
-        }}
-        thumbnails={{
-          hidden: document.documentElement.clientHeight < 800,
-          border: 0,
-          showToggle: true,
-          gap: 8,
-          padding: 0,
-          imageFit: "cover",
-        }}
-      />
-    );
-  };
-
   return (
     <>
       <Nav
@@ -160,10 +45,14 @@ const App = ({ albums }: { albums: Record<string, Album> }) => {
       />
       <Gallery
         photos={current.photos}
-        setSelectedPhoto={setSelected}
         selectedPhoto={selected}
+        setSelectedPhoto={setSelected}
       />
-      <Lightbox />
+      <Lightbox
+        photos={current.photos}
+        selectedPhoto={selected}
+        setSelectedPhoto={setSelected}
+      />
     </>
   );
 };
---
+++ src/components/Lightbox.tsx
@@ -0,0 +1,123 @@
+import dayjs from "dayjs";
+import YARL, { type SlideImage } from "yet-another-react-lightbox";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+
+import type { OriginalMetadata, Photoset } from "../types";
+import { YARL_THUMBNAIL_SIZE, fullSizes } from "../sizes";
+
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+function trimSp(s: string): string {
+  return s.replace(/\s+/g, " ").trim();
+}
+
+function trimCommonPrefixWords(base: string, toTrim: string): string {
+  const baseWords = base.split(" ");
+  const toTrimWords = toTrim.split(" ");
+  for (let i = 0; i < baseWords.length && i < toTrimWords.length; i++) {
+    if (baseWords[i] !== toTrimWords[i]) return toTrimWords.slice(i).join(" ");
+  }
+  return toTrimWords.slice(baseWords.length).join(" ");
+}
+
+function prettyMeta(m: OriginalMetadata): {
+  title: string;
+  description: string;
+} {
+  const t: string[] = [];
+  if (m.title) t.push(m.title);
+  if (m.location) t.push(m.location);
+  const title = t.join(", ");
+
+  const camera = trimSp(`${m.cameraMake || ""} ${m.cameraModel || ""}`);
+  let lens = trimSp(`${m.lensMake || ""} ${m.lensModel || ""}`);
+  lens = trimCommonPrefixWords(camera, lens);
+
+  const s: string[] = [];
+  if (m.exposureTime) s.push(`${m.exposureTime}s`);
+  if (m.fNumber) s.push(`f/${m.fNumber}`);
+  if (m.iso) s.push(`ISO ${m.iso}`);
+  const settings = s.join(", ");
+
+  const d: string[] = [];
+  if (camera) d.push(camera);
+  if (lens) d.push(lens);
+  if (settings) d.push(settings);
+  const details = d.join(", ");
+
+  if (!m.date) throw new Error(`Missing date for ${JSON.stringify(m)}`);
+
+  const description = [
+    // photoset.sizes[YARL_THUMBNAIL_SIZE].url,
+    m.description && m.description + "\n",
+    dayjs(m.date).format("dddd, MMMM D, YYYY"),
+    details,
+  ]
+    .filter(Boolean)
+    .join("\n")
+    .trim();
+
+  return { title, description };
+}
+
+const Lightbox = ({
+  photos,
+  selectedPhoto,
+  setSelectedPhoto,
+}: {
+  photos: Photoset[];
+  selectedPhoto: number | null;
+  setSelectedPhoto: (index: number | null) => void;
+}) => {
+  const slides: SlideImage[] = photos.map((photoset) => {
+    if (!photoset.sizes[YARL_THUMBNAIL_SIZE])
+      throw new Error(
+        `Missing ${YARL_THUMBNAIL_SIZE} size for ${JSON.stringify(photoset)}`
+      );
+
+    const { title, description } = prettyMeta(photoset.metadata);
+
+    const srcSet = fullSizes.map(({ size }) => {
+      const src = photoset.sizes[size];
+      if (!src)
+        throw new Error(`Missing ${size} size for ${JSON.stringify(photoset)}`);
+      return { src: src.url, width: src.width, height: src.height };
+    });
+
+    return {
+      title,
+      description,
+      srcSet,
+      src: photoset.sizes[YARL_THUMBNAIL_SIZE].url,
+      width: photoset.sizes[YARL_THUMBNAIL_SIZE].width,
+      height: photoset.sizes[YARL_THUMBNAIL_SIZE].height,
+    };
+  });
+
+  return (
+    <YARL
+      plugins={[Captions, Thumbnails]}
+      open={selectedPhoto !== null}
+      close={() => setSelectedPhoto(null)}
+      index={selectedPhoto ?? 0}
+      slides={slides}
+      captions={{
+        showToggle: true,
+        descriptionMaxLines: 8,
+        descriptionTextAlign: "center",
+      }}
+      thumbnails={{
+        border: 0,
+        showToggle: true,
+        gap: 8,
+        padding: 0,
+        imageFit: "cover",
+      }}
+    />
+  );
+};
+
+export default Lightbox;
