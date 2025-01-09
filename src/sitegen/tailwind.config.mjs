35406fa81581c00537a6488adaecf9ec8115c396
Matt Lewis <matt@mplewis.com>
2025-01-08 19:44:58 -0700

    refactor: pull tailwind screen sizes from constants

--- src/sitegen/tailwind.config.mjs
+++ src/sitegen/tailwind.config.mjs
@@ -1,20 +1,17 @@
+import { screenSizes } from "./src/sizes";
+
+const s5 = screenSizes.find(({ size }) => size === "s5");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
     screens: {
-      s3: "450px",
-      s4: "600px",
-      desktop: "750px",
-      s5: "750px",
-      s6: "900px",
-      s7: "1050px",
-      s8: "1200px",
-      s9: "1350px",
-      s10: "1500px",
-      s11: "1650px",
-      s12: "1800px",
+      desktop: `${s5.width}px`,
+      ...Object.fromEntries(
+        screenSizes.map(({ size, width }) => [size, `${width}px`])
+      ),
     },
   },
   plugins: [],
